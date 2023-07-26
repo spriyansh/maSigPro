c75a93a5af51ea23cc9f72b4dd9af4af331882a5
spriyansh <spriyansh29@gmail.com>
2023-07-26 16:11:39 +0200

    Formatted R code

--- R/i.rank.R
+++ R/i.rank.R
@@ -1,9 +1,8 @@
 "i.rank" <-
-function (x) 
-{
+  function(x) {
     xx <- x
     for (i in 1:length(xx)) {
-        xx[i] <- c(1:length(unique(x)))[x[i] == unique(x)]
+      xx[i] <- c(1:length(unique(x)))[x[i] == unique(x)]
     }
     xx
-}
+  }
--- R/reg.coeffs.R
+++ R/reg.coeffs.R
@@ -1,8 +1,7 @@
 "reg.coeffs" <-
-function(coefficients, indepen = groups.vector[nchar(groups.vector)==min(nchar(groups.vector))][1], groups.vector, 
-    group) 
-    
-{
+  function(
+      coefficients, indepen = groups.vector[nchar(groups.vector) == min(nchar(groups.vector))][1], groups.vector,
+      group) {
     coefficients <- as.numeric(coefficients)
     c0 <- coefficients[1]
     if (length(coefficients) == length(groups.vector)) groups.vector <- groups.vector[-1]
@@ -10,18 +9,16 @@ function(coefficients, indepen = groups.vector[nchar(groups.vector)==min(nchar(g
     c1 <- coefficients[groups.vector == indepen]
     c01 <- c(c0, c1)
     if (group != indepen) {
-        group <- paste(group, "vs", indepen, sep = "")
-        c2 <- coefficients[group == groups.vector]
-        if (length(c2) < length(c01)) {
-            c2 <- c(0, c2)
-        }
-        else if (length(c2) != length(c01)) {
-            stop("incomplete coeffcients")
-        }
-        reg.coeff <- c01 + c2
-    }
-    else {
-        reg.coeff <- c01
+      group <- paste(group, "vs", indepen, sep = "")
+      c2 <- coefficients[group == groups.vector]
+      if (length(c2) < length(c01)) {
+        c2 <- c(0, c2)
+      } else if (length(c2) != length(c01)) {
+        stop("incomplete coeffcients")
+      }
+      reg.coeff <- c01 + c2
+    } else {
+      reg.coeff <- c01
     }
     reg.coeff
-}
+  }
